71ad3a0190429d858660dfa0ea9683981f616e39
John Beaderstadt <jmbeaderstadt@gmail.com>
2023-01-25 17:21:27 -0600

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // Columns for the table in DB
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
